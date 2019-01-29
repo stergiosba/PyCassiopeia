0a72ba35c94e5da01f6969e56d94b92ca27b23a0
Stergios <stergiosba@gmail.com>
2019-01-29 11:30:39 +0200

    Added data and update

--- writer.py
+++ writer.py
@@ -18,7 +18,7 @@ fit_df = pd.DataFrame(columns = saved_list)
 #3600 Full Hour
 #proof
 #f_fullpath = "./S50ME-C8.2-TII - IGLC Dicle - FWE [2013-12-03 062328 00006].csv"
-f_fullpath = "./S50ME-C8.2-TII - IGLC Dicle - FWE [2014-01-02 095027 00010].csv"
+f_fullpath = "./data.csv"
 df = pd.read_csv(f_fullpath)
 #Scaling the values
 DATAS = ["FR_eng_meas","RE_HMI_ECUA_020115","RE_gov_idx_meas"]
@@ -28,57 +28,61 @@ df.loc[:,'RE_gov_idx_meas'] *= 100
 
 jack = df[['FR_eng_meas']]
 bob = df[['RE_HMI_ECUA_020115']]
-
 measurement = 'RE_HMI_ECUA_020115'
 df = df[[measurement]]
-df2 = df[df.RE_HMI_ECUA_020115.isnull()]
-#ISWS NA VRW ENAN KALUTERO TROPO
-iX_missing_Data = []
-for i in range(len(df2)):
-	iX_missing_Data.append(df2.index[i])
-for i in range(len(df)):
-	if i in iX_missing_Data:
-		df[measurement][i] = 0
-segment_size = 180
-print("You have chosen to segmentize the data per " + str(segment_size)+ " seconds.")
+print(50*"-")
+print("~$> Initializing")
+print("~$> Loading the dataset from " +f_fullpath)
+print("~$> Calculated",df[df[measurement].isnull()].size,"missing datapoints.")
+for i in df[df[measurement].isnull()].index:
+	df[measurement][i] = 0
+print("~$> All missing datapoints have been restored")
+print(50*"-")
+segment_size = 600
+print("~$> You have chosen to segmentize the data per",segment_size,"seconds.")
 segment_start = 0
-segment_end = segment_size
+segment_end = segment_size-1
 begin = time.time()
 seg_count = 0
 #plot_counts = 0
 #First Loop through the whole datafile and divide it into segments of information.
-for segment in range(0,1600,segment_size):#len(df)-segment_size,segment_size):
+for segment in range(0,segment_size,segment_size):#len(df)-segment_size,segment_size):
 	segment_df = df[segment_start:segment_end]
 	segment_df = segment_df.reset_index(drop=True)
 	windows = 0
-	if (segment_df[measurement].mean() == 0):
+	if (segment_df[measurement].mean() == -1):
 		pass
 	else:
 		#plot_counts+=1
-		window_size = 60
+		window_size = 100
 		window_start = segment_df.index.min()
-		window_end = window_start + window_size-1
+		print(window_start)
+		window_end = window_start + window_size
+		print(window_end)
 		#print(segment_df.index.max())
-		window_step = 20
+		window_step = window_size
 		fig = segment_df.plot(color='blue')
 		plt.title(r'$Engine \ Speed \ Data$', fontsize=16)
 		for window in range(segment_df.index.min(),segment_df.index.max(),window_step):
 			#print(segment_df[measurement].iloc[window_start])
 			window_df = segment_df[window_start:window_end]
-			window_df = window_df.reset_index(drop=True)	
+			window_df = window_df.reset_index(drop=True)
+			#print(window)
 			acel_df = pd.DataFrame()
 			for i in range(len(window_df)):
+				#print("Xx")
+				#print(i)
 				if i == 0:
 					acel_df = acel_df.append([0])
 					ace = 0;
 					label ="S"
 				else:
-					acel_df = acel_df.append([window_df[measurement].iloc[i]-window_df[measurement].iloc[i-1]])
 					ace = window_df[measurement].iloc[i]-window_df[measurement].iloc[i-1]
-			ave=(acel_df[0].mean()*100)
-			if ave < 0:
+					#acel_df = acel_df.append(ace)
+			#ave=(acel_df[0].mean()*100)
+			if ace < 0:
 				label ="D"
-			elif ave > 0:
+			elif ace > 0:
 				label ="A"
 			else:
 				label ="S"
@@ -88,18 +92,17 @@ for segment in range(0,1600,segment_size):#len(df)-segment_size,segment_size):
 				'N_AVE': window_df[measurement].mean(),
 				'N_IN' : segment_df[measurement].iloc[window_start],
 				'N_OUT': segment_df[measurement].iloc[len(window_df)-1],
-				'A_AVE': ave,
+				'A_AVE': ace,
 				'LABEL': label
 				},ignore_index=True)
-			plt.text((window_start+window_end)/2, window_df[measurement].iloc[window_df.index.max()], label+'\n'+str(round(ave,2)), bbox=dict(facecolor='red', alpha=0.5))
+			plt.text((window_start+window_end)/2, window_df[measurement].iloc[window_df.index.max()], label+'\n'+str(round(ace,2)), bbox=dict(facecolor='red', alpha=0.5))
 			plt.axvline(x=window_start+window_df.index.max(), color='red', linestyle=':',linewidth=1, markersize=12)
-			plt.text(window_start+window_df.index.max(), plt.ylim()[0], 1, bbox=dict(facecolor='green', alpha=0.5))
-
+			plt.text(window_start+window_df.index.max(), plt.ylim()[0], window_start, bbox=dict(facecolor='green', alpha=0.5))
 			plt.plot(window_start+window_df[measurement].idxmax(),window_df[measurement].max(),'m>') 
 			plt.plot(window_start+window_df[measurement].idxmin(),window_df[measurement].min(),'ro')
-			plt.plot(window_start,segment_df[measurement].iloc[window_start],'y<')
-			plt.plot(window_end,segment_df[measurement].iloc[len(window_df)],'gx')
-			plt.plot((window_start+window_end)/2,window_df[measurement].mean(),'r+')
+			plt.plot(window_start,window_df[measurement].iloc[0],'y<')
+			plt.plot(window_end,window_df[measurement].iloc[window_df.index.max()],'gx')
+			#plt.plot((window_start+window_end)/2,window_df[measurement].mean(),'r+')
 			#plt.text(600, window_df[measurement].mean(), ave, bbox=dict(facecolor='red', alpha=0.5))
 			#fig, axs = plt.subplots(nrows=1, ncols=1)
 			#fig, axs = plt.plot()
